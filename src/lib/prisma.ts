25ffd30befda001f93b7a83aee9e132d3e757fd2
TarcisioRodrigues <tarcisiorodrigues454@gmail.com>
2023-10-23 21:20:50 -0300

    feat:prisma and gooogleAuth

---
+++ src/lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+
+declare global {
+    var cachedPrsima: PrismaClient
+}
+
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient()
+} else {
+    if (!global.cachedPrsima) {
+        global.cachedPrsima = new PrismaClient()
+    }
+
+    prisma = global.cachedPrsima
+}
+
+
+export const prismaClient = prisma
