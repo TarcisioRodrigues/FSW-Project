61cf9df75318bb6eacf08875f2f2e6e8161b78ba
TarcisioRodrigues <tarcisiorodrigues454@gmail.com>
2023-10-25 09:37:59 -0300

    feat:adds categories and props

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Image from "next/image"
+import { Categories } from "./components/categories"
+
+export default function Home() {
+
+  return <div className="p-5">
+    <Image src="/banneroff.png" height={0} width={0} className="h-auto w-full" sizes="100vw" alt="até 55% de desconto" />
+    <div className="mt-8">
+      <Categories />
+    </div>
+  </div>
+}
