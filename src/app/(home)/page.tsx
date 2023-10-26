1958281b8145061a9766f45cfac44402bb43804a
TarcisioRodrigues <tarcisiorodrigues454@gmail.com>
2023-10-26 10:55:04 -0300

    feat:promoBanner and section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,8 @@ import Image from "next/image"
 import { Categories } from "./components/categories"
 import { prismaClient } from "@/src/lib/prisma"
 import { ProductList } from "./components/product-list"
+import { SectionTitle } from "@/src/components/ui/section"
+import { PromoBanner } from "./components/promobanner"
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -11,13 +13,26 @@ export default async function Home() {
       discountPercentage: { gt: 0 }
     }
   })
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards"
+      }
+    }
+  })
   return <div className="">
-    <Image src="/banneroff.png" height={0} width={0} className="h-auto w-full px-5" sizes="100vw" alt="até 55% de desconto" />
+    <PromoBanner src="/banneroff.png" alt="Ate 55% de desconto em mouses" />
     <div className="mt-8 px-5">
       <Categories />
     </div>
     <div className="mt-8">
+      <SectionTitle>Ofertas</SectionTitle>
       <ProductList products={deals} />
+    </div>
+    <PromoBanner src="/bannermouses.png" alt="até 50% de desconto em mouses" />
+    <div className="mt-8">
+      <SectionTitle>Teclados</SectionTitle>
+      <ProductList products={keyboards} />
     </div>
   </div>
 }
